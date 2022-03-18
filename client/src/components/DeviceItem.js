c2ec960e498ec039377951804bceade35fcaee53
Ramil Galikeev <galikeevramil95@gmail.com>
2022-03-18 13:34:50 +0500

    deleted console.log

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -8,7 +8,6 @@ import star from '../assets/star.svg';
 
 const DeviceItem = ({device}) => {
     const navigate = useNavigate();
-    console.log(navigate);
 
     return (
         <Col md={3} className='mt-3' onClick={() => navigate(DEVICE_ROUTE + '/' + device.id)} style={{'cursor': 'pointer'}}>
